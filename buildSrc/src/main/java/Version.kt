c341eddd7f772b6121415ffb8297d6b9b05796c7
bhindor <bhindor@dgsw.hs.kr>
2023-01-26 15:49:21 +0900

    🚀 :: Version
    android&jetbrains

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,4 +1,6 @@
 object Version {
-    const val ANDROID = "8.0.0-alpha11"
-    const val JETBRAINS = "1.8.0"
+//    const val ANDROID = "8.0.0-alpha11"
+//    const val JETBRAINS = "1.8.0"
+    const val ANDROID = "7.4.0"
+    const val JETBRAINS = "1.7.21"
 }
