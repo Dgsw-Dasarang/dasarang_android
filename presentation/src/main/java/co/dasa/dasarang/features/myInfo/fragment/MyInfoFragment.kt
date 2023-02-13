547c46b4e775d787beafcb76bc05f714a2ef487c
bhindor <bhindor@dgsw.hs.kr>
2023-02-13 16:29:57 +0900

    📄 :: MyInfoFragment / MyInfoViewModel
    내 정보 파일 생성

--- presentation/src/main/java/co/dasa/dasarang/features/myInfo/fragment/MyInfoFragment.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/myInfo/fragment/MyInfoFragment.kt
@@ -1,4 +1,30 @@
 package co.dasa.dasarang.features.myInfo.fragment
 
-class MyInfoFragment {
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import co.dasa.dasarang.R
+import co.dasa.dasarang.base.BaseFragment
+import co.dasa.dasarang.databinding.FragmentMyinfoBinding
+import co.dasa.dasarang.extensions.repeatOnStarted
+import co.dasa.dasarang.features.myInfo.viewmodel.MyInfoViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+
+@AndroidEntryPoint
+class MyInfoFragment : BaseFragment<FragmentMyinfoBinding, MyInfoViewModel>(R.layout.fragment_myinfo) {
+
+    override val viewModel: MyInfoViewModel by viewModels()
+    override val hasBottomNavigation: Boolean = true
+
+    override fun start() {
+        repeatOnStarted {
+            viewModel.eventFlow.collect { event -> handleEvent(event)}
+        }
+    }
+
+    private fun handleEvent(event: MyInfoViewModel.Event) {
+        when(event) {
+            is MyInfoViewModel.Event.ShowToast -> Toast.makeText(requireContext(), event.text, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
--- presentation/src/main/java/co/dasa/dasarang/features/myInfo/viewmodel/MyInfoViewModel.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/myInfo/viewmodel/MyInfoViewModel.kt
@@ -1,4 +1,27 @@
 package co.dasa.dasarang.features.myInfo.viewmodel
 
-class MyInfoViewModel {
+import androidx.lifecycle.viewModelScope
+import co.dasa.dasarang.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kr.co.moreversal.grabthathoe.utils.MutableEventFlow
+import kr.co.moreversal.grabthathoe.utils.asEventFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class MyInfoViewModel @Inject constructor() : BaseViewModel() {
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun showToast() {
+        event(Event.ShowToast("테스트 토스트"))
+    }
+
+    private fun event(event: Event) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+
+    sealed class Event {
+        data class ShowToast(val text: String) : Event()
+    }
 }
