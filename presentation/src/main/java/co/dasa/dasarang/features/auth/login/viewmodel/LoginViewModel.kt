434fc64d09cf97c4946008a37b7ccd5c8f6f4329
bhindor <bhindor@dgsw.hs.kr>
2023-02-13 16:31:26 +0900

    🐛 :: Fix Hilt Inject
    viewmodel에 inject를 추가했습니다

--- presentation/src/main/java/co/dasa/dasarang/features/auth/login/viewmodel/LoginViewModel.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/auth/login/viewmodel/LoginViewModel.kt
@@ -2,6 +2,7 @@ package co.dasa.dasarang.features.auth.login.viewmodel
 
 import co.dasa.dasarang.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel : BaseViewModel()
+class LoginViewModel @Inject constructor() : BaseViewModel()
