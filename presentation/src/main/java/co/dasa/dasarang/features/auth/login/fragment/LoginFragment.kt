4bd5549e6ccb0ccf8d1154dad4f6faa34bdccbc8
bhindor <bhindor@dgsw.hs.kr>
2023-02-16 19:52:22 +0900

    ✅ :: Login
    로그인에서 기본적인 움직임을 처리하였습니다

--- presentation/src/main/java/co/dasa/dasarang/features/auth/login/fragment/LoginFragment.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/auth/login/fragment/LoginFragment.kt
@@ -1,20 +1,62 @@
 package co.dasa.dasarang.features.auth.login.fragment
 
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import co.dasa.dasarang.R
 import co.dasa.dasarang.base.BaseFragment
 import co.dasa.dasarang.databinding.FragmentLoginBinding
+import co.dasa.dasarang.extensions.repeatOnStarted
+import co.dasa.dasarang.features.auth.join.fragment.JoinFragment
 import co.dasa.dasarang.features.auth.login.viewmodel.LoginViewModel
+import co.dasa.dasarang.features.plaza.fragment.PlazaFragment
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class LoginFragment @Inject constructor() : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layout.fragment_login) {
+class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layout.fragment_login) {
 
     override val viewModel: LoginViewModel by viewModels()
 
     override fun start() {
+        repeatOnStarted {
+            viewModel.eventFlow.collect {event -> handleEvent(event)}
+        }
+    }
 
+    private fun handleEvent(event: LoginViewModel.Event) {
+        when (event) {
+            is LoginViewModel.Event.Login -> checkLogin()
+            is LoginViewModel.Event.Join -> moveJoin()
+        }
+    }
+
+    private fun checkLogin() {
+        if (binding.etUserId.text.isNotBlank() && binding.etUserPw.text.isNotBlank()) {
+            //TODO id, pw 비교 확인 작업 후 승인
+            val fragmentManager = fragmentManager
+
+            val plazaFragment = PlazaFragment()
+            fragmentManager!!.beginTransaction().apply {
+                replace(R.id.fcv_main, plazaFragment)
+                commit()
+            }
+        }else if (binding.etUserId.text.isNullOrBlank()){
+            binding.etUserId.requestFocus()
+            Toast.makeText(requireContext(), "아이디를 입력해 주세요", Toast.LENGTH_SHORT).show()
+        }else if (binding.etUserPw.text.isNullOrBlank()) {
+            binding.etUserPw.requestFocus()
+            Toast.makeText(requireContext(), "비밀번호를 입력해 주세요", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun moveJoin() {
+        val fragmentManager = fragmentManager
+
+        val joinFragment = JoinFragment()
+        fragmentManager!!.beginTransaction().apply {
+            replace(R.id.fcv_main, joinFragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 
 }
--- presentation/src/main/java/co/dasa/dasarang/features/auth/login/viewmodel/LoginViewModel.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/auth/login/viewmodel/LoginViewModel.kt
@@ -1,8 +1,35 @@
 package co.dasa.dasarang.features.auth.login.viewmodel
 
+import androidx.lifecycle.viewModelScope
 import co.dasa.dasarang.base.BaseViewModel
+import co.dasa.dasarang.features.myInfo.viewmodel.MyInfoViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kr.co.moreversal.grabthathoe.utils.MutableEventFlow
+import kr.co.moreversal.grabthathoe.utils.asEventFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() : BaseViewModel()
+class LoginViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    fun login() {
+        event(Event.Login(1))
+    }
+
+    fun join() {
+        event(Event.Join(1))
+    }
+
+    private fun event(event: Event) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+
+    sealed class Event {
+        data class Login(val loginNum: Int) : Event()
+
+        data class Join(val joinNum: Int) : Event()
+    }
+}
