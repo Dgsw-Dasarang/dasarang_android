1808a2de9fba50f08d8c80fe1e5ee76601978278
LucyMoon <rjf5907@daum.net>
2023-03-21 14:10:38 +0900

    📄 :: add Url File

---
+++ data/src/main/java/co/dasa/data/network/url/DasaUrl.kt
@@ -0,0 +1,10 @@
+package co.dasa.data.network.url
+
+object DasaUrl {
+    const val SERVER_HOST = "https://server.saifriend.com"
+    const val LOGIN = "auth/sign-in"
+    const val JOIN_USER = "auth/user/sign-up"
+    const val JOIN_OWNER = "auth/owner/sign-up"
+
+    const val REFRESH_TOKEN = ""
+}
