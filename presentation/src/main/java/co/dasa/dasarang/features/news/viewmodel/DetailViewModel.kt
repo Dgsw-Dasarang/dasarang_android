6ab8b86b262c319fa4076edd135c5854ea6266ad
bhindor <bhindor@dgsw.hs.kr>
2023-03-27 11:21:37 +0900

    ⚡️ :: Detail View
    세부 창 뷰 추가

--- presentation/src/main/java/co/dasa/dasarang/features/news/viewmodel/DetailViewModel.kt
+++ presentation/src/main/java/co/dasa/dasarang/features/news/viewmodel/DetailViewModel.kt
@@ -1,13 +1,27 @@
 package co.dasa.dasarang.features.news.viewmodel
 
+import androidx.lifecycle.viewModelScope
 import co.dasa.dasarang.base.BaseViewModel
 import co.dasa.dasarang.features.plaza.viewmodel.PlazaViewModel
+import kotlinx.coroutines.launch
 import kr.co.moreversal.grabthathoe.utils.MutableEventFlow
 import kr.co.moreversal.grabthathoe.utils.asEventFlow
 import javax.inject.Inject
 
 class DetailViewModel @Inject constructor() : BaseViewModel() {
 
-    private val _eventFlow = MutableEventFlow<PlazaViewModel.Event>()
+    private val _eventFlow = MutableEventFlow<DetailViewModel.Event>()
     val eventFlow = _eventFlow.asEventFlow()
+
+    fun back() {
+        event(Event.Back)
+    }
+
+    private fun event(event: DetailViewModel.Event) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+
+    sealed class Event{
+        object Back : Event()
+    }
 }
