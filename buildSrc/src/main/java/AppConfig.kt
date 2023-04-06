ad1605262d0d42d75db6dedf505205ad2531157b
bhindor <bhindor@dgsw.hs.kr>
2023-04-06 22:10:07 +0900

    ✅ :: SDK Version
    sdk version

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,9 +1,9 @@
 import org.gradle.api.JavaVersion
 
 object AppConfig {
-    const val compileSdk = 33
+    const val compileSdk = 32
     const val minSdk = 26
-    const val targetSdk = 33
+    const val targetSdk = 32
     const val versionCode = 1
     const val versionName = "1.0"
 
