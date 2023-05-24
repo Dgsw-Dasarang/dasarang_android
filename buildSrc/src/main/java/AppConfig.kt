e43d25da1985d6019bf2ef6d380bfccd29bce9f9
LucyMoon <rjf5907@daum.net>
2023-05-24 21:42:23 +0900

    🚀 :: release new version

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -4,8 +4,8 @@ object AppConfig {
     const val compileSdk = 32
     const val minSdk = 26
     const val targetSdk = 32
-    const val versionCode = 3
-    const val versionName = "1.0.2"
+    const val versionCode = 4
+    const val versionName = "1.0.3"
 
     const val appId = "co.dasa.dasarang"
     const val pathData = "co.dasa.dasarang.data"
